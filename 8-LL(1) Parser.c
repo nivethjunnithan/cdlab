ed24a778757596a414d67be377c53e43757931b5
Niveth J Unnithan <nivethunnithan@gmail.com>
2020-11-24 10:54:25 +0530

    Update 8-LL(1) Parser.c

--- 8-LL(1) Parser.c
+++ 8-LL(1) Parser.c
@@ -1,86 +1,207 @@
 #include<stdio.h>
-#include<conio.h>
-void main(){
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
 
-/OPERATOR PRECEDENCE PARSER/
-char stack[20],ip[20],opt[10][10][1],ter[10];
-int i,j,k,n,top=0,col,row;
-for(i=0;i<10;i++)
+void followfirst(char , int , int);
+void findfirst(char , int , int);
+void follow(char c);
+
+int count,n=0;
+int sid;
+char calc_first[10][100];
+char calc_follow[10][100];
+int m=0;
+char production[10][10], first[10];
+char f[10];
+int k,j,land;
+char ck;
+int e;
+char ter[100];
+char table[100][100];
+int main(int argc,char **argv)
 {
- stack[i]=NULL;
- ip[i]=NULL;
- for(j=0;j<10;j++)
- {
- opt[i][j][1]=NULL;
- }
+	int jm=0;
+	int km=0;
+	int i,choice;
+	char c,ch;
+	printf("How many productions ? :");
+	scanf("%d",&count);
+	printf("\nEnter %d productions in form A=B where A and B are grammar symbols :\n\n",count);
+	for(i=0;i<count;i++)
+	{	
+		scanf("%s%c",production[i],&ch);
+	}
+	int kay;
+	char done[count];
+	int ptr = -1;
+	for(k=0;k<count;k++){
+		for(kay=0;kay<100;kay++){
+			calc_first[k][kay] = '!';
+		}
+	}
+	int point1 = 0,point2,xxx;
+
+	printf("\n\nPlease enter the desired INPUT STRING = ");
+	char input[100];
+	scanf("%s%c",input,&ch);
+	printf("\n\t\t\t\t\t===========================================================================\n");
+	printf("\t\t\t\t\t\tStack\t\t\tInput\t\t\tAction");
+	printf("\n\t\t\t\t\t===========================================================================\n");
+	int i_ptr = 0,s_ptr = 1;
+	char stack[100];
+	stack[0] = '$';
+	stack[1] = table[0][0];
+	while(s_ptr != -1){
+		printf("\t\t\t\t\t\t");
+		int vamp = 0;
+		for(vamp=0;vamp<=s_ptr;vamp++){
+			printf("%c",stack[vamp]);
+		}
+		printf("\t\t\t");
+		vamp = i_ptr;
+		while(input[vamp] != '\0'){
+			printf("%c",input[vamp]);
+			vamp++;
+		}
+		printf("\t\t\t");
+		char her = input[i_ptr];
+		char him = stack[s_ptr];
+		s_ptr--;
+		if(!isupper(him)){
+			if(her == him){
+				i_ptr++;
+				printf("POP ACTION\n");
+			}
+			else{
+				printf("\nString Not Accepted by LL(1) Parser !!\n");
+				exit(0);
+			}
+		}
+		else{
+			for(i=0;i<sid;i++){
+				if(ter[i] == her)
+					break;
+			}
+			char produ[100];
+			for(j=0;j<land;j++){
+				if(him == table[j][0]){
+					if (table[j][i+1] == '#'){
+						printf("%c=#\n",table[j][0]);
+						produ[0] = '#';
+						produ[1] = '\0';
+					}
+					else if(table[j][i+1] != '!'){
+						int mum = (int)(table[j][i+1]);
+						mum -= 65;
+						strcpy(produ,production[mum]);
+						printf("%s\n",produ);
+					}
+					else{
+						printf("\nString Not Accepted by LL(1) Parser !!\n");
+						exit(0);
+					}
+				}
+			}
+			int le = strlen(produ);
+			le = le - 1;
+			if(le == 0){
+				continue;
+			}
+			for(j=le;j>=2;j--){
+				s_ptr++;
+				stack[s_ptr] = produ[j];
+			}
+		}
+	}
+	printf("\n\t\t\t=======================================================================================================================\n");
+	if (input[i_ptr] == '\0'){
+		printf("\t\t\t\t\t\t\t\tYOUR STRING HAS BEEN ACCEPTED !!\n");
+	}
+	else
+		printf("\n\t\t\t\t\t\t\t\tYOUR STRING HAS BEEN REJECTED !!\n");
+	printf("\t\t\t=======================================================================================================================\n");
 }
-printf("Enter the no.of terminals :\n");
-scanf("%d",&n);
-printf("\nEnter the terminals :\n");
-scanf("%s",&ter);
-printf("\nEnter the table values :\n");
-for(i=0;i<n;i++)
+
+void follow(char c)
 {
- for(j=0;j<n;j++)
- {
- printf("Enter the value for %c %c:",ter[i],ter[j]);
- scanf("%s",opt[i][j]);
- }
+	int i ,j;
+	if(production[0][0]==c){
+ 		f[m++]='$';
+ 	}
+ 	for(i=0;i<10;i++)
+ 	{
+  		for(j=2;j<10;j++)
+  		{
+   			if(production[i][j]==c)
+   			{
+    			if(production[i][j+1]!='\0'){
+					followfirst(production[i][j+1],i,(j+2));
+ 				}
+    			if(production[i][j+1]=='\0'&&c!=production[i][0]){
+     				follow(production[i][0]);
+				}
+   			}   
+  		}
+ 	}
 }
-printf("\n** OPERATOR PRECEDENCE TABLE **\n");
-for(i=0;i<n;i++)
+
+void findfirst(char c ,int q1 , int q2)
 {
-printf("\t%c",ter[i]);
+	int j;
+	if(!(isupper(c))){
+		first[n++]=c;
+	}
+	for(j=0;j<count;j++)
+	{
+		if(production[j][0]==c)
+		{
+			if(production[j][2]=='#'){
+				if(production[q1][q2] == '\0')
+					first[n++]='#';
+				else if(production[q1][q2] != '\0' && (q1 != 0 || q2 != 0))
+				{
+					findfirst(production[q1][q2], q1, (q2+1));
+				}
+				else
+					first[n++]='#';
+			}
+			else if(!isupper(production[j][2])){
+				first[n++]=production[j][2];
+			}
+			else {
+				findfirst(production[j][2], j, 3);
+			}
+		}
+	}	
 }
-printf("\n");
-for(i=0;i<n;i++){printf("\n%c",ter[i]);
-for(j=0;j<n;j++){printf("\t%c",opt[i][j][0]);}}
-stack[top]='$';
-printf("\nEnter the input string:");
-scanf("%s",ip);
-i=0;
-printf("\nSTACK\t\t\tINPUT STRING\t\t\tACTION\n");
-printf("\n%s\t\t\t%s\t\t\t",stack,ip);
-while(i<=strlen(ip))
+
+void followfirst(char c, int c1 , int c2)
 {
-for(k=0;k<n;k++)
-{
-if(stack[top]==ter[k])
-col=k;
-if(ip[i]==ter[k])
-row=k;
-}
-if((stack[top]=='$')&&(ip[i]=='$')){
-printf("String is accepted\n");
-break;}
-else if((opt[col][row][0]=='<') ||(opt[col][row][0]=='='))
-{ stack[++top]=opt[col][row][0];
-stack[++top]=ip[i];
- printf("Shift %c",ip[i]);
- i++;
-  }
-else{
- if(opt[col][row][0]=='>')
-{
-while(stack[top]!='<'){--top;}
-top=top-1;
-printf("Reduce");
-}
-else
-{
-printf("\nString is not accepted");
-break;
-}
-}
-printf("\n");
-for(k=0;k<=top;k++)
-{
-printf("%c",stack[k]);
-}
-printf("\t\t\t");
-for(k=i;k<strlen(ip);k++){
-printf("%c",ip[k]);
-}
-printf("\t\t\t");
-}
+    int k;
+    if(!(isupper(c)))
+		f[m++]=c;
+	else{
+		int i=0,j=1;
+		for(i=0;i<count;i++)
+		{
+			if(calc_first[i][0] == c)
+				break;
+		}
+		while(calc_first[i][j] != '!')
+		{
+			if(calc_first[i][j] != '#'){
+				f[m++] = calc_first[i][j];
+			}
+			else{
+				if(production[c1][c2] == '\0'){
+					follow(production[c1][0]);
+				}
+				else{
+					followfirst(production[c1][c2],c1,c2+1);
+				}
+			}
+			j++;
+		}
+	}
 }
